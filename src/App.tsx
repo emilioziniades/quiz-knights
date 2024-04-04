dbb08cd5adbe1120bd5bdb53f36ba7466b64f4e8
Emilio Ziniades <emilioziniades@protonmail.com>
2024-04-04 22:54:32 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1> The Quiz Knights</h1>
+      <div className="card">
+        <p>How did we do?</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
