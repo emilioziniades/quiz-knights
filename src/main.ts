b71eef1006f5d5f23045e39846cb163bd1603238
Emilio Ziniades <emilioziniades@protonmail.com>
2024-04-05 22:11:42 +0200

    switch to vite+vanilla-ts

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import "./style.css";
+import data from "./data.json";
+import { Category, labels } from "./types.ts";
+import { average, titleCase } from "./utils.ts";
+import Chart from "chart.js/auto";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+      <h1>The Quiz Knights</h1>
+      <p>How did we do?</p>
+      <h2>Average Score By Category</h2>
+      <canvas id="averagePerCategory">
+      <h2>Placement Across Rounds</h2>
+      <h2>Score Across Rounds</h2>
+  </div>
+`;
+
+const ctx = document.querySelector<HTMLButtonElement>("#averagePerCategory")!;
+
+const averages = labels.map((label: Category) =>
+  average(data.map((datum) => datum.category_scores[label])),
+);
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: labels.map(titleCase),
+    datasets: [
+      {
+        label: "Average Score By Category",
+        data: averages,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+  },
+});
