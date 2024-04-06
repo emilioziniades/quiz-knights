9505aa9ffafc8fc14dd57f5da17815cc025609df
Emilio Ziniades <emilioziniades@protonmail.com>
2024-04-06 19:28:13 +0200

    first chart done

--- src/charts.ts
+++ src/charts.ts
@@ -2,6 +2,10 @@ import { Category, labels } from "./types";
 import data from "./data.json";
 import { average, titleCase } from "./utils";
 import Chart from "chart.js/auto";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+Chart.register(ChartDataLabels);
+Chart.defaults.color = "#FEFAE0";
 
 export function renderAveragePerCategory(element: HTMLCanvasElement) {
   const averages = labels.map((label: Category) =>
@@ -16,18 +20,39 @@ export function renderAveragePerCategory(element: HTMLCanvasElement) {
         {
           label: "Average Score By Category",
           data: averages,
-          backgroundColor: "rgba(255, 99, 132, 0.5)",
         },
       ],
     },
     options: {
       responsive: true,
+      scales: {
+        y: {
+          min: 0,
+          max: 5,
+          grid: {
+            display: true,
+          },
+          ticks: {
+            maxTicksLimit: 6,
+          },
+        },
+        x: {
+          grid: {
+            display: false,
+          },
+        },
+      },
       plugins: {
         legend: {
           display: false,
         },
         title: {
           display: false,
+        },
+        datalabels: {
+          anchor: "end",
+          align: "bottom",
+          formatter: (x, _) => x.toFixed(2),
         },
       },
     },
